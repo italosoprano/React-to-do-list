10993162c9934ee2a1f53ea7d174b717cb93a635
Ítalo Soprano <110266373+italosoprano@users.noreply.github.com>
2023-08-01 18:30:11 -0400

    update

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,22 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import Todo from "./componentes/Todo"
 import Form from "./componentes/Form";
 import FilterButton from "./componentes/FilterButton";
 
 function App(props) {
 
-  const taskList = props.tasks.map((task)=> (
+  const [tasks, setTasks] = useState(props.tasks)
+
+  const taskList = tasks.map((task)=> (
   <Todo 
   id={task.id} 
   name={task.name} 
   completed={task.completed} 
   key={task.id}/>
   ));
+
+  function addTask(name){
+    const newTask = {id: 'id', name, completed: false};
+    setTasks([...tasks, newTask]);
+  }
   
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form/>
+      <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
         <FilterButton/>
         <FilterButton/>
